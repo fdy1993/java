724676816ed2d0db1587da1761ed6268382ed149
fandeyi <fdy19930621@163.com>
2020-10-08 14:58:06 +0800

    fdy

--- HelloWrod.java
+++ HelloWrod.java
@@ -5,5 +5,8 @@ public class HelloWrod {
 
     public static void main(String[] args) {
         System.out.println("Hell, Word");
+        System.out.println("-----------");
+        int a = 9527;
+        System.out.println(a);
     }
 }
