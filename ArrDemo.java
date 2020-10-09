f905495149d220b4835dc04c7a1667371f763948
fandeyi <fdy19930621@163.com>
2020-10-09 22:10:32 +0800

    数组

---
+++ ArrDemo.java
@@ -0,0 +1,8 @@
+public class ArrDemo {
+    public static void main(String[] args) {
+        int[] arr = new int [3];
+        arr[2] = 100;
+        System.out.println(arr[2]);
+        System.out.println(arr[1]);
+    }
+}
