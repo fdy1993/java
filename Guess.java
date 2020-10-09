e811b9cda73737789bc0f4b52aaebb2d612fbfc8
fdy <fdy19930621@163.com>
2020-10-09 11:07:58 +0800

    猜数字游戏

---
+++ Guess.java
@@ -0,0 +1,26 @@
+/**
+ * Guess
+ */
+import java.util.Random;
+import java.util.Scanner;
+public class Guess {
+public static void main(String[] args) {
+    Random randomnumber = new Random();
+    int randomnum = randomnumber.nextInt(100) + 1;
+    Scanner guess = new Scanner(System.in);
+    while (true) {
+        System.out.println("请输入你猜的数字：");
+        int guessnum = guess.nextInt();
+        if (guessnum < randomnum) {
+            System.out.println("你猜的数字" +guessnum+ "小了");
+        } else if (guessnum>randomnum) {
+            System.out.println("你猜的数字" +guessnum+ "大了");
+        } else {
+            System.out.println("恭喜你猜对了！");
+            break;
+        }
+    }
+    guess.close();
+}
+    
+}
