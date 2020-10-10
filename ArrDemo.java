c241d33aa984824e28ec5f4e1aadda74a6b5e450
fandeyi <fdy19930621@163.com>
2020-10-10 20:41:35 +0800

    数组

--- ArrDemo.java
+++ ArrDemo.java
@@ -4,5 +4,7 @@ public class ArrDemo {
         arr[2] = 100;
         System.out.println(arr[2]);
         System.out.println(arr[1]);
+        int[] arr2 = {1, 2, 3, 5};
+        System.out.println(arr2[3]);
     }
 }
