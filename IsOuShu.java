8fa7c0ddc0be716810a3ab7e1a98c85b77e311d6
fandeyi <fdy19930621@163.com>
2020-10-11 22:12:03 +0800

    方法定义及调用

---
+++ IsOuShu.java
@@ -0,0 +1,11 @@
+public class IsOuShu {
+    public static void main(String[] args) {
+        isOuShuMa();
+    }
+    public static void isOuShuMa() {
+        int num = 20;
+        if (num%2==0) {
+            System.out.println(num + "偶数");
+        }
+    }
+}
