1252d1d262a177e53aca57d0e54f03e417550965
fandy <fdy19930621@gmail.com>
2020-10-21 22:40:28 +0800

    数组遍历,未完成

---
+++ MethodTest.java
@@ -0,0 +1,17 @@
+public class MethodTest {
+    public static void main(String[] args) {
+        int [] arr = {11, 22, 33, 44, 55};
+        printArr(arr);
+    }
+    // public static void printArr(int[] arr) {
+    //     for (int x=0; x<arr.length; x++) {
+    //         System.out.println(arr[x]);
+    //     }
+    // }
+    public static void printArr(int[] arr) {
+        for (int x=0; x<arr.length; x++) {
+            System.out.println(arr[x]);
+        }
+    }
+
+}
